d49f2ffb39b0c0670dce2e56f5d2aeab75baeb36
Grace kungu <grayckungu2@gmail.com>
2023-09-03 17:24:47 +0300

    update changes

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', 1),
+       ('Rowdy', 3, 'M', 'Black', 'Playful', 1),
+       ('Tabitha', 4, 'F', 'Gray', 'Calm', 1),
+       ('Sergeant Brown', 7, 'M', 'Brown', 'Aggressive', 1),
+       ('Melissa', 2, 'F', 'White', 'Shy', 1),
+       ('Grinch', 6, 'M', 'Green', 'Grumpy', 1),
+       ('Wendy', 8, 'F', 'Brown', 'Energetic', 1),
+       ('Unnamed', NULL, NULL, NULL, NULL, NULL, 1);
--- lib/test_setup.py
+++ lib/test_setup.py
@@ -1,2 +1,35 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from lib.sql_queries import (
+    select_all_female_bears_return_name_and_age,
+    select_all_bears_names_and_orders_in_alphabetical_order,
+    select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest,
+    select_oldest_bear_and_returns_name_and_age,
+    select_youngest_bear_and_returns_name_and_age,
+)
+
+def test_select_all_female_bears_return_name_and_age():
+    # Write test logic here to verify the correctness of the query
+    pass
+
+def test_select_all_bears_names_and_orders_in_alphabetical_order():
+    # Write test logic here to verify the correctness of the query
+    pass
+
+def test_select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest():
+    # Write test logic here to verify the correctness of the query
+    pass
+
+def test_select_oldest_bear_and_returns_name_and_age():
+    # Write test logic here to verify the correctness of the query
+    pass
+
+def test_select_youngest_bear_and_returns_name_and_age():
+    # Write test logic here to verify the correctness of the query
+    pass
+
+# Run the tests
+if __name__ == "__main__":
+    import pytest
+    pytest.main() 
